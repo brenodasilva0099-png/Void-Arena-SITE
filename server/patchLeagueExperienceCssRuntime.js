a6e411141947c27e6f06434cbb1ca5c7caec882e
brenodasilva0099-png <brenodasilva0099@gmail.com>
2026-07-19 15:30:23 -0300

    fix(site): keep final profile patch after integrity

--- server/patchLeagueExperienceCssRuntime.js
+++ server/patchLeagueExperienceCssRuntime.js
@@ -1,7 +1,6 @@
 const fs = require('node:fs');
 const path = require('node:path');
 require('./patchLeagueMenuDedupRuntime');
-require('./patchLeagueProfilesCompetitionsHomeRuntime');
 
 const file = path.join(__dirname, '..', 'public', 'css', 'league-experience.css');
 if (!fs.existsSync(file)) process.exit(0);
