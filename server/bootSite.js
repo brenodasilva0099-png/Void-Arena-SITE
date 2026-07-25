85b18a7548a5240da4721581d5e67c4c0f7e49a9
brenodasilva0099-png <brenodasilva0099@gmail.com>
2026-07-25 17:57:26 -0300

    fix(invites): ativar entrega canônica nos correios

--- server/bootSite.js
+++ server/bootSite.js
@@ -63,6 +63,7 @@ const patches = [
   './patchDiscordLegalPlacarRuntime',
   './patchBracketStylesheetFinalRuntime',
   './patchFormsStaticAssetRuntime',
+  './patchRecruitmentNotificationDeliveryRuntime',
   './patchFinalNavigationNoPrefetchRuntime'
 ];
 
