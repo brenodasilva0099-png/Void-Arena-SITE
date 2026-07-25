cf480a661e3052398254f80dece133b5f7fd711c
brenodasilva0099-png <brenodasilva0099@gmail.com>
2026-07-25 18:15:58 -0300

    chore(times): ativar convite por link no boot

--- server/bootSite.js
+++ server/bootSite.js
@@ -65,6 +65,7 @@ const patches = [
   './patchFormsStaticAssetRuntime',
   './patchRecruitmentNotificationDeliveryRuntime',
   './patchTeamMemberManagementAndCssRuntime',
+  './patchTeamInviteLinksRuntime',
   './patchFinalNavigationNoPrefetchRuntime'
 ];
 
